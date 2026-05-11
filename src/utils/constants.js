47806885c8ead2392cdbdc11c239d15b18e9fb48
aboodnassar-hub <aboodnassar68@gmail.com>
2026-05-12 00:28:57 +0300

    URL modified

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://evopath-backend.onrender.com";
 
 export const INITIAL_ACTIVITIES = [
   {
